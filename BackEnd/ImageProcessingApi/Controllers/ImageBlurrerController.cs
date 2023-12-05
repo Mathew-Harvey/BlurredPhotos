5c653f742066228e918dc3bc8c5b9053f5b4948c
Mat Harvey <mat.harvey@rise-x.io>
2023-12-05 10:23:35 +0800

    change image library to imageSharp

--- BackEnd/ImageProcessingApi/Controllers/ImageBlurrerController.cs
+++ BackEnd/ImageProcessingApi/Controllers/ImageBlurrerController.cs
@@ -1,45 +1,43 @@
-using System;
-using System.Drawing;
 using Microsoft.AspNetCore.Mvc;
-using Services.ImageBlurrer; // Ensure this matches the actual namespace of ImageBlurrer
 using Microsoft.AspNetCore.Http;
-using System.IO;
+using Services.ImageBlurrer; // Ensure this matches the actual namespace of ImageBlurrer
 using System.Threading.Tasks;
-using System.Drawing.Imaging;
+using SixLabors.ImageSharp; // Add ImageSharp namespace
+using SixLabors.ImageSharp.PixelFormats; // Add ImageSharp PixelFormats namespace
 
 [ApiController]
 [Route("[controller]")]
 public class ImageController : ControllerBase
 {
-[HttpPost("upload")]
-public async Task<IActionResult> UploadImage(IFormFile file)
-{
-    if (file == null || file.Length == 0)
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadImage(IFormFile file)
     {
-        return BadRequest("No file uploaded.");
-    }
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("No file uploaded.");
+        }
 
-    try
-    {
-        ImageBlurrer blurrer = new ImageBlurrer();
-        Bitmap originalImage = await blurrer.ConvertToBitmapAsync(file);
+        try
+        {
+            ImageBlurrer blurrer = new ImageBlurrer();
+            Image<Rgba32> originalImage = await blurrer.ConvertToImageAsync(file); // Use Image<Rgba32>
 
-        Console.WriteLine("UploadImage: Image conversion successful.");
+            Console.WriteLine("UploadImage: Image conversion successful.");
 
-        Bitmap blurredImage = blurrer.BlurImage(originalImage, 70); // Adjust the radius as needed
+            Image<Rgba32> blurredImage = blurrer.BlurImage(originalImage, 20); // Adjust the radius as needed
 
-        Console.WriteLine("UploadImage: Image blurring successful.");
+            Console.WriteLine("UploadImage: Image blurring successful.");
 
-        var imageBytes = blurrer.SaveImageToStream(blurredImage, ImageFormat.Png);
-      
-        return File(imageBytes, "image/png");
-
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"UploadImage: Error - {ex.Message}");
-        return StatusCode(500, "Internal server error: " + ex.Message);
+            using (var imageStream = blurrer.SaveImageToStream(blurredImage, "png")) // Use "png" as string
+            {
+                return File(imageStream.ToArray(), "image/png");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"UploadImage: Error - {ex.Message}");
+            return StatusCode(500, "Internal server error: " + ex.Message);
+        }
     }
 }
 
-}
